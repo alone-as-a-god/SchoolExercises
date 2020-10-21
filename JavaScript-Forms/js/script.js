076d8fa28df3223681fa0a9b7f2abe461575ddf2
Jonathan Seyr <jonathan.seyr_br@snets.it>
2020-10-21 09:48:11 +0200

    ex 2 finished

--- JavaScript-Forms/js/script.js
+++ JavaScript-Forms/js/script.js
@@ -2,10 +2,17 @@ const monthNames = ["January", "February", "March", "April", "May", "June",
   "July", "August", "September", "October", "November", "December"
 ];
 
+////////////EXERCISE 1 //////////////
+
 function clickSubmit(){
     username = document.getElementById('usernameInput').value;
     window.alert(username);
+    
 }
+/////////////////////////////////////
+
+
+///////// EXERCISE 2 ///////////////
 
 function updateTime(){
     
@@ -16,5 +23,29 @@ function updateTime(){
     hour = date.getHours();
     minute = date.getMinutes();
     second = date.getSeconds();
-    window.alert(year + " " + monthNames[month] + " " + day + "th " + hour + " " +minute + " " + second);
-}
+    millisecond = date.getMilliseconds();
+    prefix = "am";
+    if(hour >= 12){
+      prefix = "pm";
+    }
+    if(millisecond < 100){
+      millisecond = "0"+ parseInt(millisecond);
+    }else{
+      millisecond = parseInt(millisecond);
+    }
+    if(second < 10){
+      second = "0"+parseInt(second);
+    }
+    if(minute < 10){
+      minute = "0"+parseInt(minute);
+    }
+
+    //window.alert(monthNames[month] + " " + day + "th " + year + " " + hour + ":" +minute + ":" + second + " " + prefix);
+    document.getElementById("time").innerHTML = monthNames[month] + " " + day + "th " + year + " " + hour + ":" +minute + ":" + second + ":" + millisecond + " " + prefix;
+  }
+
+window.setInterval(function(){
+  updateTime();
+}, 1);
+
+//////////////////////////////////////////////////
