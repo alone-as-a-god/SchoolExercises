b4ef2db5dff8c10a49342959756319c43541e360
Jonathan Seyr <jonathan.seyr_br@snets.it>
2020-10-21 08:17:03 +0200

    added ex2

--- JavaScript-Forms/js/script.js
+++ JavaScript-Forms/js/script.js
@@ -1,6 +1,20 @@
-username = document.getElementById('usernameInput').value;
+const monthNames = ["January", "February", "March", "April", "May", "June",
+  "July", "August", "September", "October", "November", "December"
+];
 
 function clickSubmit(){
-    username = lelele;
+    username = document.getElementById('usernameInput').value;
     window.alert(username);
+}
+
+function updateTime(){
+    
+    date = new Date();
+    year = date.getFullYear();
+    month = date.getMonth();
+    day = date.getDay()
+    hour = date.getHours();
+    minute = date.getMinutes();
+    second = date.getSeconds();
+    window.alert(year + " " + monthNames[month] + " " + day + "th " + hour + " " +minute + " " + second);
 }
